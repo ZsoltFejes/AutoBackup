944340bf552cc50b4dcb8a3c33fb995bff7cad5b
Zsolt Fejes <zsolt.fejes@icloud.com>
2021-06-07 20:04:33 +0100

    Added feature to check the latest archive and see if source has been modified since then

--- main.go
+++ main.go
@@ -4,63 +4,99 @@ import (
 	"archive/zip"
 	"flag"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"time"
 )
 
-func main() {
-	destination := flag.String("destination", "", "Specify destination directory and/or filename for the archive. If you don't specify the name of the destination, source directory's name will be used")
-	source := flag.String("source", "", "Specify the path to the directory you want to archive")
-	flag.Parse()
+var (
+	source        string
+	destination   string
+	w             *zip.Writer
+	archiveNeeded = false
+)
 
+func isDirectory(path string) {
+	// Check if source is a directory
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		log.Fatalf("There was an error checking the directory '%s'! Make sure it exist\n", path)
+	}
+	if !fileInfo.IsDir() {
+		log.Fatalf("%s is not a directory", path)
+	}
+}
+
+// Sanitises inputs and checks source directory
+func runChecks() {
+	var (
+		archiveNameBase string    // Used to check simmilarly names archives
+		lastArchived    time.Time // Used to store the last archive's timestamp
+	)
 	// Check if source directory is specified
-	if len(*source) == 0 {
+	if len(source) == 0 {
 		log.Fatalf("Missing required --source parameter")
 	}
 
-	// Check if source is a directory
-	fileInfo, err := os.Stat(*source)
-	if err != nil {
-		log.Fatalln("There was an error checking the source directory\n" + err.Error())
-	}
-	if !fileInfo.IsDir() {
-		log.Fatalf("%s is not a directory", *source)
-	}
+	// Check and make sure source is a directory, script curently doesn't support single file to be archived
+	isDirectory(source)
 
 	// Get the source directory name
-	s := strings.Split(*source, `/`) // Linux
+	s := strings.Split(source, `/`) // Linux
 	if len(s) == 1 {
-		s = strings.Split(*source, `\`) // Windows
+		s = strings.Split(source, `\`) // Windows
 	}
 	sourceDirectoryName := s[len(s)-2]
 
 	// Generating a string of timestamp
-	now := time.Now().Format("2006_01_02_15_04_00")
+	timeFormat := "2006_01_02T15_04"
+	now := time.Now().Format(timeFormat)
 
-	// Checks if destination was specified, if not, source directory name will be used as base of destination file name
-	if len(*destination) == 0 {
-		*destination = sourceDirectoryName + "&" + now + ".zip"
-	} else if strings.HasSuffix(*destination, ".zip") {
-		*destination = strings.TrimSuffix(*destination, ".zip")
-		*destination = *destination + "&" + now + ".zip"
-	} else if !strings.HasSuffix(*destination, ".zip") {
-		*destination = *destination + "/" + sourceDirectoryName + "&" + now + ".zip"
+	// Check if destination was specified, if no the name of the destination will be the same as the source's directory name also the destination will be the current directory
+	// If .zip extention is used then that name will be used to generate the archive
+	// If directory is specified as destnation the name of the source will be used as the name of the archive and will be saved to the specified destination directory
+	if len(destination) == 0 {
+		archiveNameBase = sourceDirectoryName
+		destination = archiveNameBase + "&" + now + ".zip"
+	} else if strings.HasSuffix(destination, ".zip") {
+		archiveNameBase = strings.TrimSuffix(destination, ".zip")
+		destination = archiveNameBase + "&" + now + ".zip"
+	} else if !strings.HasSuffix(destination, ".zip") {
+		isDirectory(destination)
+		if runtime.GOOS == "windows" {
+			destination = destination + `\`
+		} else {
+			destination = destination + "/"
+		}
+		archiveNameBase = sourceDirectoryName
+		destination = destination + sourceDirectoryName + "&" + now + ".zip"
 	}
 
-	// Creating archive file
-	file, err := os.Create(*destination)
+	// Get files in destination
+	files, err := ioutil.ReadDir(filepath.Dir(destination))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	defer file.Close()
 
-	w := zip.NewWriter(file)
-	defer w.Close()
+	// Check each file in destnation directory. If the file name matches the new archive's base name the last modified timestamp of it will  be stored.
+	for _, fileInfo := range files {
+		archiveFound, err := filepath.Match(archiveNameBase+"*", fileInfo.Name())
+		if err != nil {
+			log.Fatalf("There was an error matching archive %s\n", fileInfo.Name())
+		}
+		if archiveFound {
+			// If newer archive is found it's timestamp will be recorded
+			if lastArchived.Before(fileInfo.ModTime()) {
+				lastArchived = fileInfo.ModTime()
+			}
+		}
+	}
 
-	walker := func(path string, info os.FileInfo, err error) error {
+	sourceWalker := func(path string, info os.FileInfo, err error) error {
 		// Check if any error was passed to walker, and pass it back to main function
 		if err != nil {
 			return err
@@ -71,37 +107,91 @@ func main() {
 			return nil
 		}
 
-		// Opens source file
-		file, err := os.Open(path)
+		// Getting file info
+		fileInfo, err := os.Stat(path)
 		if err != nil {
-			log.Printf("Unable to open file: %s\n", path)
-			return err
-		}
-		defer file.Close()
-
-		// Converting from path to zip-root relative path
-		path = strings.TrimPrefix(path, *source)
-
-		// Creating file in archive
-		f, err := w.Create(path)
-		if err != nil {
-			log.Printf("There was an error creating file in archive: %s\n", path)
-			return err
+			log.Fatalf("There was an error checking archive %s\n", path)
 		}
 
-		// Copying source file to zip io-writer
-		_, err = io.Copy(f, file)
-		if err != nil {
-			log.Printf("There was an error copying file: %s\n", path)
-			return err
+		// If there is a file that was modified after the latest archive was created archive needed will be set to true
+		if fileInfo.ModTime().After(lastArchived) {
+			archiveNeeded = true
 		}
 
 		return nil
 	}
 
-	// Walk through all files inside of specified source directory including all sub directories
-	err = filepath.Walk(*source, walker)
+	// Go through and check each file in source if archive is needed
+	err = filepath.Walk(source, sourceWalker)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func archiveWalker(path string, info os.FileInfo, err error) error {
+	// Check if any error was passed to walker, and pass it back to main function
+	if err != nil {
+		return err
+	}
+
+	// Skip rest of the function if directory
+	if info.IsDir() {
+		return nil
+	}
+
+	// Opens source file
+	file, err := os.Open(path)
+	if err != nil {
+		log.Printf("Unable to open file: %s\n", path)
+		return err
+	}
+	defer file.Close()
+
+	// Converting from path to zip-root relative path
+	path = strings.TrimPrefix(path, source)
+
+	// Creating file in archive
+	f, err := w.Create(path)
+	if err != nil {
+		log.Printf("There was an error creating file in archive: %s\n", path)
+		return err
+	}
+
+	// Copying source file to zip io-writer
+	_, err = io.Copy(f, file)
+	if err != nil {
+		log.Printf("There was an error copying file: %s\n", path)
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	flag.StringVar(&destination, "destination", "", "Specify destination directory and/or filename for the archive. If you don't specify the name of the destination, source directory's name will be used")
+	flag.StringVar(&source, "source", "", "Specify the path to the directory you want to archive")
+	flag.Parse()
+
+	runChecks()
+
+	// Walk through all files inside of specified source directory including all sub directories
+	if archiveNeeded {
+		// Creating archive file
+		file, err := os.Create(destination)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		w = zip.NewWriter(file)
+		defer w.Close()
+		// Archive each file in source directory
+		err = filepath.Walk(source, archiveWalker)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("%s has been archived to %s", source, destination)
+	} else {
+		log.Println("There have been no changes in source since last archive")
 	}
 }
